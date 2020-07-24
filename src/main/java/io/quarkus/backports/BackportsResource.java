4dfbe73309c2c281407170edcef26f49add420b7
George Gastaldi <gegastaldi@gmail.com>
2020-07-24 14:02:39 -0300

    Refactoring

--- src/main/java/io/quarkus/backports/BackportsResource.java
+++ src/main/java/io/quarkus/backports/BackportsResource.java
@@ -1,55 +1,27 @@
 package io.quarkus.backports;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueState;
-import org.kohsuke.github.GHMilestone;
-import org.kohsuke.github.GHPullRequest;
-import org.kohsuke.github.GHPullRequestCommitDetail;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
-import org.kohsuke.github.PagedSearchIterable;
-
 import io.quarkus.qute.TemplateExtension;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkus.qute.api.CheckedTemplate;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import org.kohsuke.github.GHMilestone;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHPullRequestCommitDetail;
 
 @Path("/")
 public class BackportsResource {
 
-    @ConfigProperty(name = "backports.repository")
-    String repositoryName;
-
-    @ConfigProperty(name = "backports.label")
-    String backportLabel;
-
-    private GitHub github;
-
-    private GHRepository repository;
-
-    @PostConstruct
-    public void initialize() {
-        try {
-            this.github = GitHubBuilder.fromPropertyFile().build();
-            this.repository = github.getRepository(repositoryName);
-        } catch (IOException e) {
-            throw new IllegalStateException("Unable to connect to GitHub API", e);
-        }
-    }
+    @Inject
+    GitHubService gitHub;
 
     @CheckedTemplate
     public static class Templates {
@@ -61,32 +33,15 @@ public class BackportsResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance index() throws IOException {
-        return Templates.index(repository.listMilestones(GHIssueState.OPEN).toList());
+        return Templates.index(gitHub.getOpenMilestones());
     }
 
     @GET
     @Path("/backports/{milestoneId}/")
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance backports(@PathParam Integer milestoneId) throws IOException {
-        GHMilestone milestone = getMilestone(milestoneId);
-
-        PagedSearchIterable<GHIssue> issues = github.searchIssues().isClosed()
-                .q("is:pr")
-                .q("label:" + backportLabel)
-                .q("repo:" + repositoryName)
-                .list()
-                .withPageSize(1000);
-
-        // Unfortunately, I haven't found a proper way to get GHPullRequest objects with a query
-        // and we have too many closed ones to get them all and filter them locally
-        List<GHPullRequest> pullRequests = new ArrayList<>();
-
-        for (GHIssue issue : issues) {
-            pullRequests.add(repository.getPullRequest(issue.getNumber()));
-        }
-
-        Collections.sort(pullRequests, PullRequestComparator.INSTANCE);
-
+        GHMilestone milestone = gitHub.getMilestone(milestoneId);
+        List<GHPullRequest> pullRequests = gitHub.getBackportCandidatesPullRequests();
         return Templates.backports(milestone, pullRequests);
     }
 
@@ -94,25 +49,10 @@ public class BackportsResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/backports/{milestoneId}/backported/{pullRequestId}/")
     public String markAsBackported(@PathParam Integer milestoneId, @PathParam Integer pullRequestId) throws IOException {
-        GHMilestone milestone = getMilestone(milestoneId);
-        GHPullRequest pullRequest = repository.getPullRequest(pullRequestId);
-
-        // this doesn't seem to work :/
-        pullRequest.setMilestone(milestone);
-        pullRequest.removeLabels(backportLabel);
-
-        // we also need to affect the milestone to all the potentially linked issues
-        // I haven't looked if there is already an API to get them (without parsing the content of the message)
-
+        GHMilestone milestone = gitHub.getMilestone(milestoneId);
+        GHPullRequest pullRequest = gitHub.getPullRequest(pullRequestId);
+        gitHub.markPullRequestAsBackported(pullRequest, milestone);
         return "SUCCESS";
-    }
-
-    private GHMilestone getMilestone(Integer milestoneId) {
-        try {
-            return repository.getMilestone(milestoneId);
-        } catch (IOException e) {
-            throw new IllegalStateException("Unable to find milestone " + milestoneId, e);
-        }
     }
 
     @TemplateExtension
@@ -130,16 +70,6 @@ public class BackportsResource {
 
         static String abbreviatedSha(GHPullRequestCommitDetail commit) {
             return commit.getSha().substring(0, 7);
-        }
-    }
-
-    private static class PullRequestComparator implements Comparator<GHPullRequest> {
-
-        private static PullRequestComparator INSTANCE = new PullRequestComparator();
-
-        @Override
-        public int compare(GHPullRequest o1, GHPullRequest o2) {
-            return o1.getMergedAt().compareTo(o2.getMergedAt());
         }
     }
 }
---
+++ src/main/java/io/quarkus/backports/GitHubService.java
@@ -0,0 +1,91 @@
+package io.quarkus.backports;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHMilestone;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.kohsuke.github.PagedSearchIterable;
+
+@ApplicationScoped
+public class GitHubService {
+
+    private final GitHub gitHub;
+
+    private final GHRepository repository;
+
+    private final String backportLabel;
+
+    @Inject
+    public GitHubService(
+            @ConfigProperty(name = "backports.repository")
+                    String repositoryName,
+            @ConfigProperty(name = "backports.label")
+                    String backportLabel) throws IOException {
+        this.gitHub = GitHubBuilder.fromPropertyFile().build();
+        this.repository = gitHub.getRepository(repositoryName);
+        this.backportLabel = backportLabel;
+    }
+
+    public GHPullRequest getPullRequest(int pullRequestId) throws IOException {
+        return repository.getPullRequest(pullRequestId);
+    }
+
+    public List<GHMilestone> getOpenMilestones() throws IOException {
+        return repository.listMilestones(GHIssueState.OPEN).toList();
+    }
+
+    public GHMilestone getMilestone(Integer milestoneId) throws IOException {
+        return repository.getMilestone(milestoneId);
+    }
+
+    public List<GHPullRequest> getBackportCandidatesPullRequests() throws IOException {
+        PagedSearchIterable<GHIssue> issues = gitHub.searchIssues().isClosed()
+                .q("is:pr")
+                .q("label:" + backportLabel)
+                .q("repo:" + repository.getName())
+                .list()
+                .withPageSize(1000);
+
+        // Unfortunately, I haven't found a proper way to get GHPullRequest objects with a query
+        // and we have too many closed ones to get them all and filter them locally
+        List<GHPullRequest> pullRequests = new ArrayList<>();
+        for (GHIssue issue : issues) {
+            pullRequests.add(repository.getPullRequest(issue.getNumber()));
+        }
+        pullRequests.sort(PullRequestComparator.INSTANCE);
+        return pullRequests;
+    }
+
+
+    public void markPullRequestAsBackported(GHPullRequest pullRequest, GHMilestone milestone) throws IOException {
+        // this doesn't seem to work :/
+        pullRequest.setMilestone(milestone);
+        pullRequest.removeLabels(backportLabel);
+
+        // we also need to affect the milestone to all the potentially linked issues
+        // I haven't looked if there is already an API to get them (without parsing the content of the message)
+    }
+
+    private enum PullRequestComparator implements Comparator<GHPullRequest> {
+        INSTANCE;
+
+        @Override
+        public int compare(GHPullRequest o1, GHPullRequest o2) {
+            return o1.getMergedAt().compareTo(o2.getMergedAt());
+        }
+    }
+
+
+}
