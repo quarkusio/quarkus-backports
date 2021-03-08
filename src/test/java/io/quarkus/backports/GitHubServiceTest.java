b3f43f6c22853cc005d64ed3419bbcc81b8ca76d
Guillaume Smet <guillaume.smet@gmail.com>
2021-03-08 11:46:41 +0100

    Reorganize imports properly

--- src/test/java/io/quarkus/backports/GitHubServiceTest.java
+++ src/test/java/io/quarkus/backports/GitHubServiceTest.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import javax.inject.Inject;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 @Disabled("CI needs to be properly configured")
